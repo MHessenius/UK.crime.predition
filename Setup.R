84080c99a8f480d0e49a4f0c400797e3fef76ce5
“MHessenius” <mhessenius@web.de>
2017-06-05 21:29:12 +0200

    minor changes

--- Setup.R
+++ Setup.R
@@ -1,18 +1,23 @@
 ## Setup
-
 file_names <- dir("C:/Users/hessenim.hub/Downloads/01_Final/2012_01/")
-setwd("C:/Users/hessenim.hub/Downloads/01_Final/2012_01/")
+setwd("C:/Users/hessenim.hub/Downloads/01_Final/2012_01/") #exact working director necessary for the do.call command
 crime <- do.call(rbind,lapply(file_names,read.csv))
 setwd("H:/GitHub/UK.crime.predition")
 
 ## CLEANING
-crime <- crime[!crime$Reported.by=="Police Service of Northern Ireland",] 
+crime <- crime[!crime$Reported.by=="Police Service of Northern Ireland",]
+crime <- crime[!is.na(crime$Longitude),] #no location = no LSOA.code
 
+# -> sapply(crime, function(x){sum(is.na(x))}) check for NAs
+
+sum(crime$LSOA.code=="") #those are not countes as NAs, but Longitude / Latitude is given
+crime <- crime[!crime$LSOA.code=="",]
 
 ## Gsub to get the names of the 348 local authorities
 crime$authority <- gsub(" [0-9]..*", "", crime$LSOA.name)
 
-# --> create a second naming for a possible easier matching
+# --> create a second frame with slightly different names
+# for a possible easier matching
 crime$authority_v2 <- crime$authority  
 
 # renaming
